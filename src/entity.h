5b64dc6a13989621abc6d1bd07c2d126f3bd2945
Roland Marchand <roland.marchand@protonmail.com>
2026-04-10 05:36:29 -0400

    Added character idea for the game

--- src/entity.h
+++ src/entity.h
@@ -21,6 +21,24 @@ enum {
 	TYPE_CELESTIAL = 1 << 7,
 };
 
+/* There should be some dude named Jerry. He kind of just talks to you. He went
+ * to college with the player. They did sports ball together, and did some
+ * naughty stuff in the players' bathroom. Only once, it was awkward and never
+ * talked about again. You didn't forget, but you think Jerry might have. He did
+ * not. Little did you know, Jerry broke up with all of his exes because
+ * whenever he had sex with them, he could only think of you. You are okay with
+ * this. Jerry is actually a cool dude, and you can see yourself raising
+ * children together. Jerry is the right role model, and you are the wrong one.
+ * Ying and yang. But unfortunately, despite destiny having woven your paths
+ * together, you're just not over your ex, and your ex wasn't written in the
+ * destiny so it kind of just sucks for you. While you talk to Jerry, you replay
+ * that evening in the showers with him for the thousandth time. Each time was
+ * better than the last. Jerry talks to you about his new hot sauce idea for the
+ * salad bar. You don't like salad, but you'll try it anyway. For Jerry. Jerry's
+ * sauce.
+ *
+ * — Haru */
+
 _Static_assert((TYPE_BEAST | TYPE_UNDEAD | TYPE_DEMON | TYPE_ELEMENTAL |
                 TYPE_FUNGUS | TYPE_HUMAN | TYPE_KOBOLD | TYPE_CELESTIAL) <= UINT8_MAX,
                "EntityType flags exceed uint8_t range");
